3fb7cf9180ee5da56d88455f728e686d547804de
piotrbecker <piotrbecker@users.noreply.github.com>
2021-03-03 14:34:04 +0100

    Update runUAT2.sh
    app_stage change

--- section_4/code/cd_pipeline/tests/runUAT2.sh
+++ section_4/code/cd_pipeline/tests/runUAT2.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+
+# set variables
+hostname=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' app_stage)
+port=$1
+
+# wait for the app to start
+sleep 5 
+
+# ping the app
+status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
+
+if [ $status_code == 200 ];
+then
+	echo "PASS: ${hostname}:${port} is reachable"
+else
+	echo "FAIL: ${hostname}:${port} is unreachable"
+    exit 1
+fi
